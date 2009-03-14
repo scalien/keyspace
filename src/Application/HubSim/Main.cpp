c7121268e435a259acaf61c0c08781f1d25567e5
agazso <agazso@scalien.com>
2009-03-14 16:04:24 +0000

    Added HubSim.

---
+++ src/Application/HubSim/Main.cpp
@@ -0,0 +1,24 @@
+#include "System/Events/EventLoop.h"
+#include "System/IO/IOProcessor.h"
+#include "HubSim.h"
+
+IOProcessor* ioproc;
+
+int main(int argc, char* argv[])
+{
+	HubSim hubSim;
+	EventLoop* eventloop;
+	
+	Log_SetTimestamping(true);
+	
+	ioproc = IOProcessor::New();
+	eventloop = new EventLoop(ioproc);
+	
+	ioproc->Init();
+	
+	hubSim.CreateNode("127.0.0.1:4001", 5000);
+	hubSim.CreateNode("127.0.0.1:4000", 5001);
+	
+	eventloop->Run();
+	return 0;
+}	
