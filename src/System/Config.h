7e6f3c4ed3611b4238df3662c134347b285cad1c
mtrencseni <mtrencseni@scalien.com>
2009-05-17 14:38:48 +0000

    Reorg.

--- src/System/Config.h
+++ src/System/Config.h
@@ -15,29 +15,3 @@ public:
 };
 
 #endif
-/*
-
-paxos.nodeID = 0
-paxos.endpoints = 127.0.0.1:10000, 127.0.0.1:10010, 127.0.0.1:10020
-
-database.dir = test/0/
-database.type = hash # or btree
-
-http.port = 8080
-memcached.port = 11111
-
-====================
-C++ interface usage:
-====================
-Config::Init(configfile);
-
-PaxosConfig::nodeID = Config::GetIntValue("paxos.nodeID", 0);
-PaxosConfig::numNodes = Config::GetListNum("paxos.endpoints");
-for (int i = 0; i < PaxosConfig::numNodes; i++)
-{
-	PaxosConfig::endpoints[i].Set(Config::GetListValue("paxos.endpoints", i, NULL));
-}
-
-database.Init(Config::GetValue("database.dir"));
-
-*/
