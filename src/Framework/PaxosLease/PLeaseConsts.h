94350995e45fb25ec75c3f56f78f1530ea4d80f1
mtrencseni <mtrencseni@scalien.com>
2009-05-22 18:54:20 +0000

    Increased PaxosLease timeout to 7sec.

--- src/Framework/PaxosLease/PLeaseConsts.h
+++ src/Framework/PaxosLease/PLeaseConsts.h
@@ -5,7 +5,7 @@
 
 #define PLEASE_BUFSIZE			(1*KB)
 
-#define MAX_LEASE_TIME			3000	// msec
+#define MAX_LEASE_TIME			7000	// msec
 #define	MAX_CLOCK_SKEW			100		// msec
 
 #define PLEASE_PORT_OFFSET		1
