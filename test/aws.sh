9a872fe0c0f164d978ce26ebfde4024dfaac662e
agazso <agazso@scalien.com>
2009-06-07 17:35:56 +0000

    Added assume-yes option to apt-get in aws.sh.

--- test/aws.sh
+++ test/aws.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apt-get update
-apt-get install subversion libdb4.6++-dev joe telnet unzip gdb
+apt-get -y install subversion libdb4.6++-dev joe telnet unzip gdb
 svn checkout http://svn.scalien.com/keyspace/trunk keyspace
 cd keyspace
 make
