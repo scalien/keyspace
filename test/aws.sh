3604944481a9d2591e38dabb7187c8bfc1cae4fb
agazso <agazso@scalien.com>
2009-06-07 17:33:03 +0000

    Added Amazon EC2 script.

---
+++ test/aws.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+apt-get update
+apt-get install subversion libdb4.6++-dev joe telnet unzip gdb
+svn checkout http://svn.scalien.com/keyspace/trunk keyspace
+cd keyspace
+make
+make clientlib
+g++ -Isrc -o bin/clienttest src/Test/KeyspaceClientTest.cpp bin/clientlib.a
