9897f60eb9f9a15fca0858f647d4ba60f657e743
mtrencseni <mtrencseni@scalien.com>
2009-06-23 00:20:48 +0000

    Fixed LIST bug.

--- src/Application/Keyspace/Database/AsyncListVisitor.cpp
+++ src/Application/Keyspace/Database/AsyncListVisitor.cpp
@@ -7,6 +7,7 @@ AsyncVisitorCallback::AsyncVisitorCallback()
 
 	numkey = 0;
 	op = NULL;
+	complete = false;
 }
 
 AsyncVisitorCallback::~AsyncVisitorCallback()
@@ -64,17 +65,17 @@ void AsyncVisitorCallback::Execute()
 	{
 		// HACK in order to not copy the buffer we set the members of
 		// the key and value manually and set it back before deleting 
-		/*op->key.buffer = keys[i].buffer;
+		op->key.buffer = keys[i].buffer;
 		op->key.size = keys[i].size;
-		op->key.length = keys[i].length;*/
+		op->key.length = keys[i].length;
 		
 		if (op->type == KeyspaceOp::LISTP || op->type == KeyspaceOp::DIRTY_LISTP)
 		{
 			// this is a huge hack, since op->value is a ByteBuffer!
 			// if it were allocated, this would result in memleak
-			/*op->value.buffer = valuebuf.buffer + valuepos[i];
+			op->value.buffer = valuebuf.buffer + valuepos[i];
 			op->value.size = valuelen[i];
-			op->value.length = valuelen[i];*/
+			op->value.length = valuelen[i];
 		}
 		
 		op->service->OnComplete(op, false);
