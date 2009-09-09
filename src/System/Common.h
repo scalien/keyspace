2ebba93774ca78681d18a77e3bc8d61f156b4e53
mtrencseni <mtrencseni@scalien.com>
2009-09-09 15:54:02 +0000

    Reformatted.

--- src/System/Common.h
+++ src/System/Common.h
@@ -45,7 +45,8 @@ inline bool Xor(bool a, bool b, bool c) { return (((int)a + (int)b + (int)c) == 
 
 inline bool Xor(bool a, bool b) { return Xor(a, b, false); }
 
-inline unsigned NumLen(int n) { return n == 0 ? 1 : (unsigned) floor(log10(n) + 1); }
+inline unsigned NumLen(int n)
+{ return n == 0 ? 1 : (unsigned) floor(log10(n) + 1); }
 
 void BlockSignals();
 
