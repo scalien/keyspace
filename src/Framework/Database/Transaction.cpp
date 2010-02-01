c6b212e83633dd4686eb533608f0f052ffce0b71
mtrencseni <mtrencseni@scalien.com>
2010-02-01 13:28:07 +0000

    DB_TXN_SYNC

--- src/Framework/Database/Transaction.cpp
+++ src/Framework/Database/Transaction.cpp
@@ -35,7 +35,7 @@ bool Transaction::IsActive()
 
 bool Transaction::Begin()
 {
-	if (database->env.txn_begin(NULL, &txn, DB_TXN_NOSYNC) != 0)
+	if (database->env.txn_begin(NULL, &txn, DB_TXN_SYNC) != 0)
 		return false;
 	
 	active = true;
@@ -46,7 +46,7 @@ bool Transaction::Commit()
 {
 	active = false;
 	
-	if (txn->commit(DB_TXN_NOSYNC) != 0)
+	if (txn->commit(DB_TXN_SYNC) != 0)
 		return false;
 	
 	return true;
