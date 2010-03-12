80b9a6866e0d26bdbd64a3ed8b14360c26827565
agazso <agazso@scalien.com>
2010-03-12 15:27:11 +0000

    Fixed C++ SWIG Wrapper interface.

--- src/Application/Keyspace/Client/KeyspaceClientWrap.cpp
+++ src/Application/Keyspace/Client/KeyspaceClientWrap.cpp
@@ -1,51 +1,239 @@
 #include "KeyspaceClientWrap.h"
 #include "KeyspaceClient.h"
 
-Client Create()
+void ResultBegin(ResultObj result_)
+{
+	Keyspace::Result*	result = (Keyspace::Result*) result_;
+
+	if (result)		
+		result->Begin();
+}
+
+void ResultNext(ResultObj result_)
+{
+	Keyspace::Result*	result = (Keyspace::Result*) result_;
+	
+	if (result)
+		result->Next();	
+}
+
+bool ResultIsEnd(ResultObj result_)
+{
+	Keyspace::Result*	result = (Keyspace::Result*) result_;
+	
+	if (!result)
+		return true;
+
+	return result->IsEnd();
+}
+
+void ResultClose(ResultObj result_)
+{
+	Keyspace::Result*	result = (Keyspace::Result*) result_;
+	
+	if (result)
+		result->Close();
+}
+
+std::string ResultKey(ResultObj result_)
+{
+	Keyspace::Result*	result = (Keyspace::Result*) result_;
+	ByteString			key;
+	int					status;
+	std::string			ret;
+	
+	if (!result)
+		return ret;
+	
+	status = result->Key(key);
+	if (status < 0)
+		return ret;
+	
+	ret.append(key.buffer, key.length);
+	
+	return ret;
+}
+
+std::string ResultValue(ResultObj result_)
+{
+	Keyspace::Result*	result = (Keyspace::Result*) result_;
+	ByteString			value;
+	int					status;
+	std::string			ret;
+
+	if (!result)
+		return ret;
+	
+	status = result->Value(value);
+	if (status < 0)
+		return ret;
+	
+	ret.append(value.buffer, value.length);
+
+	return ret;
+}
+
+ClientObj Create()
 {
 	return new Keyspace::Client();
 }
 
-int	Init(Client client_, std::list<std::string> nodes)
+int	Init(ClientObj client_, const NodeParams& params)
 {
-	Keyspace::Client *client = (Keyspace::Client *) client_;
-	int	status;
-	const char **nodev;
-	int nodec;
-	int i;
-	
-	nodec = nodes.size();
-	nodev = new const char*[nodec];
-	i = 0;
-	for (std::list<std::string>::iterator it = nodes.begin(); it != nodes.end(); it++)
-	{
-		nodev[i] = (*it).c_str();
-		i++;
-	}
-	
-	status = client->Init(nodec, nodev);
-	delete[] nodev;
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	int					status;
+
+	Log_SetTrace(true);
+	Log_SetTarget(LOG_TARGET_STDOUT);
+	status = client->Init(params.nodec, params.nodes);
 	
 	return status;
 }
 
-ReturnValue Get(Client client_, std::string key_)
+ResultObj GetResult(ClientObj client_)
 {
-	Keyspace::Client *client = (Keyspace::Client *) client_;
-	ByteString key;
-	ByteString value;
-	ReturnValue retval;
-	Keyspace::Result *result;
-	
-	key.buffer = (char*) key_.c_str();
-	key.size = key_.length();
-	key.length = key_.length();
-	
-	retval.status = client->Get(key);
-	result = client->GetResult();
-	result->Value(value);
-	
-	retval.value.append(value.buffer, value.length);
-	
-	return retval;
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+		
+	return client->GetResult();
 }
+
+int Get(ClientObj client_, const std::string& key_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	ByteString			key(key_.length(), key_.length(), key_.c_str());
+	
+	return client->Get(key);
+}
+
+uint64_t Count(ClientObj client_, 
+				  const std::string& prefix_,
+				  const std::string& startKey_,
+				  uint64_t count_,
+				  bool next_,
+				  bool forward_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	ByteString			prefix(prefix_.length(), prefix_.length(), prefix_.c_str());
+	ByteString			startKey(startKey_.length(), startKey_.length(), startKey_.c_str());
+	uint64_t			res;
+		
+	client->Count(res, prefix, startKey, count_, next_, forward_);
+	return res;
+}
+
+int	ListKeys(ClientObj client_, 
+			 const std::string& prefix_,
+			 const std::string& startKey_,
+			 uint64_t count_,
+			 bool next_,
+			 bool forward_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	ByteString			prefix(prefix_.length(), prefix_.length(), prefix_.c_str());
+	ByteString			startKey(startKey_.length(), startKey_.length(), startKey_.c_str());
+		
+	return client->ListKeys(prefix, startKey, count_, next_, forward_);
+}
+
+int	ListKeyValues(ClientObj client_, 
+			 const std::string& prefix_,
+			 const std::string& startKey_,
+			 uint64_t count_,
+			 bool next_,
+			 bool forward_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	ByteString			prefix(prefix_.length(), prefix_.length(), prefix_.c_str());
+	ByteString			startKey(startKey_.length(), startKey_.length(), startKey_.c_str());
+		
+	return client->ListKeyValues(prefix, startKey, count_, next_, forward_);
+}
+
+int Set(ClientObj client_, const std::string& key_, const std::string& value_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	ByteString			key(key_.length(), key_.length(), key_.c_str());
+	ByteString			value(value_.length(), value_.length(), value_.c_str());
+
+	return client->Set(key, value);
+}
+
+int TestAndSet(ClientObj client_, 
+			   const std::string& key_,
+			   const std::string& test_,
+			   const std::string& value_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	ByteString			key(key_.length(), key_.length(), key_.c_str());
+	ByteString			test(test_.length(), test_.length(), test_.c_str());
+	ByteString			value(value_.length(), value_.length(), value_.c_str());
+
+	return client->TestAndSet(key, test, value);
+}
+
+int64_t Add(ClientObj client_, const std::string& key_, int64_t num_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	ByteString			key(key_.length(), key_.length(), key_.c_str());
+	int64_t				result;
+	
+	client->Add(key, num_, result);
+
+	return result;
+}
+
+int Delete(ClientObj client_, const std::string& key_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	ByteString			key(key_.length(), key_.length(), key_.c_str());
+	
+	return client->Delete(key); 
+}
+
+int Remove(ClientObj client_, const std::string& key_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	ByteString			key(key_.length(), key_.length(), key_.c_str());
+	
+	return client->Remove(key); 
+}
+
+int	Rename(ClientObj client_, const std::string& from_, const std::string& to_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	ByteString			from(from_.length(), from_.length(), from_.c_str());
+	ByteString			to(to_.length(), to_.length(), to_.c_str());
+	
+	return client->Rename(from, to); 
+}
+
+int Prune(ClientObj client_, const std::string& prefix_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	ByteString			prefix(prefix_.length(), prefix_.length(), prefix_.c_str());
+	
+	return client->Prune(prefix);
+}
+
+int Begin(ClientObj client_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	
+	return client->Begin();
+}
+
+int Submit(ClientObj client_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	
+	return client->Submit();
+}
+
+int Cancel(ClientObj client_)
+{
+	Keyspace::Client*	client = (Keyspace::Client *) client_;
+	
+	return client->Cancel();
+}
+
+
--- src/Application/Keyspace/Client/KeyspaceClientWrap.h
+++ src/Application/Keyspace/Client/KeyspaceClientWrap.h
@@ -1,22 +1,95 @@
 #ifndef KEYSPACE_CLIENT_WRAP_H
 #define KEYSPACE_CLIENT_WRAP_H
 
+// SWIG compatible wrapper interface for generating client wrappers
+
 #include <string>
-#include <list>
+#include <vector>
 
-typedef void * Client;
-typedef void * Result;
+typedef void * ClientObj;
+typedef void * ResultObj;
 
-struct ReturnValue
+struct NodeParams
 {
-	std::string	value;
-	int			status;
+	NodeParams(int nodec_)
+	{
+		i = 0;
+		nodec = nodec_;
+		nodes = new const char*[nodec];
+	}
+	
+	~NodeParams()
+	{
+		Close();
+	}
+	
+	void Close()
+	{
+		delete[] nodes;
+		nodes = NULL;
+		i = 0;
+	}
+	
+	void AddNode(const char* node)
+	{
+		if (i > nodec)
+			return;
+		nodes[i++] = node;
+	}
+
+	int				nodec;
+	const char**	nodes;
+	int				i;
 };
 
-Client Create();
+void		ResultBegin(ResultObj result);
+void		ResultNext(ResultObj result);
+bool		ResultIsEnd(ResultObj result);
+void		ResultClose(ResultObj result);
+std::string	ResultKey(ResultObj result);
+std::string	ResultValue(ResultObj result);
 
-int			Init(Client client, std::list<std::string> nodes);
-ReturnValue Get(Client client, std::string key);
-//int			Set(Client client, std::string &key, std::string &value);
+ClientObj	Create();
+int			Init(ClientObj client, const NodeParams &params);
+ResultObj	GetResult(ClientObj);
+
+int			Get(ClientObj client, const std::string& key);
+//ReturnValue DirtyGet(ClientObj client, const std::string& key);
+uint64_t	Count(ClientObj client, 
+					 const std::string& prefix,
+					 const std::string& startKey,
+					 uint64_t count,
+					 bool next,
+					 bool forward);
+
+int			ListKeys(ClientObj client, 
+					 const std::string& prefix,
+					 const std::string& startKey,
+					 uint64_t count,
+					 bool next,
+					 bool forward);
+
+int			ListKeyValues(ClientObj client, 
+					 const std::string& prefix,
+					 const std::string& startKey,
+					 uint64_t count,
+					 bool next,
+					 bool forward);
+					 
+int			Set(ClientObj client, const std::string& key, const std::string& value);
+int			TestAndSet(ClientObj client, 
+					   const std::string& key,
+					   const std::string& test,
+					   const std::string& value);
+int64_t		Add(ClientObj client, const std::string& key, int64_t num);
+int			Delete(ClientObj client, const std::string& key);
+int			Remove(ClientObj client, const std::string& key);
+int			Rename(ClientObj client, const std::string& from, const std::string& to);
+int			Prune(ClientObj client, const std::string& prefix);
+
+// grouping write commands
+int			Begin(ClientObj client);
+int			Submit(ClientObj client);
+int			Cancel(ClientObj client);
 
 #endif
