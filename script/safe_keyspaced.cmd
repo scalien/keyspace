5f1a509e4bcacc48fc99ace513091dd896ccf3e5
agazso <agazso@scalien.com>
2010-03-24 14:07:55 +0000

    Removed database deletion from safe script.

--- script/safe_keyspaced.cmd
+++ script/safe_keyspaced.cmd
@@ -4,13 +4,5 @@
 if %ERRORLEVEL% == 0 goto end
 if %ERRORLEVEL% == 1 goto end
 if %ERRORLEVEL% == 143 goto end
-if %ERRORLEVEL% == 2 goto delete_database
-goto start
-:delete_database
-set KEYSPACE_DIR=%3
-if "%KEYSPACE_DIR%" == "" set KEYSPACE_DIR=%~dp2
-del %KEYSPACE_DIR%\__*
-del %KEYSPACE_DIR%\log*
-del %KEYSPACE_DIR%\keyspace
 goto start
 :end
