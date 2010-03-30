6a362f0ec362bc90e56837e22ee1929ef0d6cdde
agazso <agazso@scalien.com>
2010-03-30 15:57:25 +0000

    Fixed php test client.

--- src/Application/Keyspace/Client/PHP/test.php
+++ src/Application/Keyspace/Client/PHP/test.php
@@ -4,7 +4,7 @@
 @include_once("keyspace_client.php");
 
 //keyspace_client::Keyspace_SetTrace(TRUE);
-$ks = new Keyspace(array("localhost:7080"));
+$ks = new KeyspaceClient(array("localhost:7080"));
 print($ks->get("hol"));
 print($ks->count(array("count" => 10)));
 $ks->set("c", 2147483647);
@@ -14,7 +14,7 @@ var_dump($ks->listKeyValues(array("count" => 10)));
 var_dump($ks->listKeys(array("count" => 10)));
 var_dump($ks->listKeys(array("start_key" => "a", "count" => 10)));
 
-$ks2 = new Keyspace(array("localhost:7080"));
+$ks2 = new KeyspaceClient(array("localhost:7080"));
 print($ks2->get("hol"));
 
 ?>
