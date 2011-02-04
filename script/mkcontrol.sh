a99f24a81b4f6f8e48f012e4467cc45df7cb7a6a
Attila Gazso <agazso@scalien.com>
2011-02-04 21:08:15 +0100

    Fixed debian control script.

--- script/mkcontrol.sh
+++ script/mkcontrol.sh
@@ -12,7 +12,7 @@ Package: $PACKAGE
 Version: $VERSION
 Section: database
 Priority: optional
-Depends: libstdc++6, libevent1, libdb4.4++|libdb4.5++|libdb4.6++
+Depends: libstdc++6, libdb4.4++|libdb4.5++|libdb4.6++
 Architecture: $ARCH
 Installed-Size: 1024
 Maintainer: Scalien Software (info@scalien.com)
